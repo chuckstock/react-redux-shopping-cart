4b48853044b10122bbaab46421a1510a846219db
Blackstock <charlieblackstock@gmail.com>
2016-02-18 20:44:41 -0700

    added render teas and success message

--- src/components/tea-list.js
+++ src/components/tea-list.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchTeas } from '../actions/index';
-import axios from 'axios';
+
+import TeaDetail from './tea-detail';
 
 class TeaList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            checkoutBag: [],
+            success: false
+        };
+
+        this.updateCheckoutBag = this.updateCheckoutBag.bind(this);
+    }
+
     componentWillMount() {
         this.props.fetchTeas();
     }
 
+    renderTeas() {
+        if (!this.props.teas) {
+            return <div>Loading...</div>
+        }
+
+        return this.props.teas.map((tea) => {
+            return (
+                <TeaDetail onAddClick={this.updateCheckoutBag} key={tea._id} tea={tea} />
+            );
+        });
+    }
+
+    updateCheckoutBag(tea, quantity) {
+        this.setState({
+            checkoutBag: this.state.checkoutBag.concat({ tea, quantity }),
+            success: true
+        });
+
+        setTimeout(() => {
+            this.setState({ success: false });
+        }, 3000);
+    }
+
+    renderAlertMessage() {
+        return (
+            <div className="alert alert-success">
+                Success! Your item has been added to your cart!
+            </div>
+        );
+    }
+
     render() {
         console.log(this.props.teas);
+        console.log(this.state);
         return (
             <div>
                 <h1>Welcome to Tea Shopping Cart</h1>
+
+                {this.state.success ? this.renderAlertMessage() : null}
+
                 <table className="table table-bordered">
                     <thead>
                         <tr>
@@ -21,24 +68,17 @@ class TeaList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.renderList()}
+                        {this.renderTeas()}
                     </tbody>
                 </table>
             </div>
         );
     }
 
-    renderList() {
-        return (
-            <tr>
-                <td>hi</td>
-            </tr>
-        );
-    }
+
 }
 
 function mapStateToProps(state) {
-    console.log(state);
     return { teas: state.teas.all }
 }
 
